0f28a9fe1e77ca78eb2c133f2bbec7aae64e6bc9
T.Hashimoto <tkrhsmt@gmail.com>
2026-04-29 13:06:14 +0900

    add Transformation test

--- test/runtests.jl
+++ test/runtests.jl
@@ -22,3 +22,24 @@ end
     @test_throws ArgumentError Apoblast.Param.Library(model, (u + z,))
     @test_throws ArgumentError Apoblast.Param.Library(model, (w,))
 end
+
+@testset "Apoblast.jl / Transformation" begin
+    model = Model(("x", "y"), ("u", "v"))
+    x, y = model.coords
+    u, v = model.fields
+
+    reflection = Transformation(model, (x, -y), (u, -v))
+    @test reflection.coords_replace == (x, -y)
+    @test reflection.fields_replace == (u, -v)
+    @test reflection.coords_replace_dict == Dict(x => x, y => -y)
+    @test reflection.fields_replace_dict == Dict(u => u, v => -v)
+    @test reflection.parameter == ()
+    @test reflection.jacobian_inverse == Sym[1 0; 0 -1]
+
+    a = Sym("a")
+    shifted = Transformation(model, (x + a, -y), (u + a, -v); parameter = (a,))
+    @test shifted.parameter == (a,)
+    @test shifted.coords_replace_dict == Dict(x => x + a, y => -y)
+    @test shifted.fields_replace_dict == Dict(u => u + a, v => -v)
+    @test shifted.jacobian_inverse == Sym[1 0; 0 -1]
+end
